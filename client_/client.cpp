9523290aeba8c151da4ea77708d69749d889b787
Rivares <rivares@protonmail.com>
2024-02-15 23:25:36 +0300

    Refactoring Client

--- client_/client.cpp
+++ client_/client.cpp
@@ -1,122 +1,137 @@
-#include <bits/stdc++.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-   
-#define PORT     5200 
-   
+
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <memory>
+#include <vector>
+#include <array>
+
+using namespace boost::asio::ip;
+
 
 struct DzlPacketData
 {
-    std::array<uint8_t, 30> data;
+	std::array<uint8_t, 30> data;
 
-    DzlPacketData() {
-    }
+	DzlPacketData() {
+	}
 };
 
 #pragma pack(1)
 struct DzlPacket
 {
 
-    uint64_t        timestamp; // время цикла
-    uint64_t        number; // номер пакета
-    std::array<uint8_t, 1>  data; // данные
+	uint64_t		timestamp; // время цикла
+	uint64_t		number; // номер пакета
+	std::array<uint8_t, 1>	data; // данные
 
-    DzlPacket() {
-        timestamp = 0;
-        number = 0;
-    }
+	DzlPacket() {
+		timestamp = 0;
+		number = 0;
+	}
 };
 #pragma pack()
 
 
-struct ClockCycler
+#include <future> 
+
+class Client
 {
+private:    
+    udp::socket m_socket;
+    udp::endpoint m_endpoint;
+
+	std::array<DzlPacketData, 50>	m_packets;	
+
     std::atomic<bool> isStarted;
     std::chrono::system_clock::time_point startPoint;
     std::chrono::system_clock::time_point stopPoint;
 
     std::atomic<std::chrono::microseconds> timeCycle;
 
-    ClockCycler() = default;
+    std::future<uint64_t> func;
 
-    void startMetr()
+public:
+    Client(boost::asio::io_service& service)
+        : m_socket(service, udp::endpoint(udp::v4(), 0))
     {
-        startPoint = std::chrono::high_resolution_clock::now();
-        isStarted.store(true);
+        m_endpoint = udp::endpoint(address::from_string("127.0.0.1"), 5200);
     }
 
-    void stopMetr()
+    void startSend()
     {
-        if (isStarted.load())
-        {
-            stopPoint = std::chrono::high_resolution_clock::now();
-            timeCycle.store(std::chrono::duration_cast<std::chrono::microseconds>(stopPoint - startPoint));
+	    m_socket.async_send_to(boost::asio::buffer(&(m_packets[0].data), sizeof(DzlPacketData)),
+							m_endpoint,
+							boost::bind(&Client::handSend,
+								this,
+								boost::asio::placeholders::error,
+								boost::asio::placeholders::bytes_transferred
+									)
+							);
+    }
 
-            isStarted.store(false);
+    void handSend(const boost::system::error_code& error, std::size_t size)
+    {
+	    if (error) {
+            return;
         }
-    }
 
-    std::chrono::microseconds getTimeCycle() const
+        auto th = std::thread([&](){
+            if (isStarted.load())
+            {
+                stopPoint = std::chrono::high_resolution_clock::now();
+                timeCycle.store(std::chrono::duration_cast<std::chrono::microseconds>(stopPoint - startPoint));
+
+                isStarted.store(false);
+            }
+            startPoint = std::chrono::high_resolution_clock::now();
+            isStarted.store(true);
+
+            printf("= %lu \n"
+            , timeCycle.load().count());
+        });
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+        th.join();
+
+
+        startSend();        
+    }
+};
+
+class Base
+{
+    boost::asio::io_service service;
+    std::shared_ptr<Client> m_client;
+    std::thread m_thread;
+
+public:
+    Base()
     {
-        return timeCycle.load();
+        m_client = std::make_shared<Client>(service);
+        m_thread = std::thread(&Base::runThread, this);
+    }
+    void runThread()
+    {
+        m_client->startSend();
+        service.run();
+    }
+    ~Base()
+    {
+        if (m_thread.joinable())
+        {   m_thread.join();    }
+
+        service.stop();
     }
 };
 
 
-
 int main(int argc, char *argv[])
 {
-    int sockfd; 
-    char buffer[30]; 
-    const char *hello = "Hello from client"; 
-    struct sockaddr_in     servaddr; 
-
-    ClockCycler m_timeMeter;
-
-
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-
-    memset(&servaddr, 0, sizeof(servaddr)); 
-       
-    // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_addr.s_addr = INADDR_ANY;   
-
-
-
-    for(;;)
-    {
-        if (m_timeMeter.isStarted)
-        {
-            m_timeMeter.stopMetr();
-
-            printf("= %lu \n"
-             , m_timeMeter.getTimeCycle().count()); // TODO / YURIY / Delete
-        }
-        m_timeMeter.startMetr();
-
-        sendto(sockfd, (const char *)hello, strlen(hello), 
-            0, (const struct sockaddr *) &servaddr,  
-                sizeof(servaddr));
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(5));
-    }
-
-
-    close(sockfd);
+    Base obj;
 
     return 0;
 }
-
-
-
